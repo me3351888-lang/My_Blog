76a9588b6c0ab6ff3ccd061793f05381f5e7702e
me3351888-lang <me3351888@gmail.com>
2026-01-22 23:03:09 +0200

    Remove commented Arabic text from main.js

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { auth } from './firebase/config' // نادى على الفايربيز
+import { auth } from './firebase/config' 
 import { onAuthStateChanged } from 'firebase/auth'
 
 let app;
 
-// استنى لما فايربيز يتأكد من المستخدم الأول
+
 onAuthStateChanged(auth, () => {
   if (!app) {
     app = createApp(App)
