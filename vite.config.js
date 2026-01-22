8a623ca76f40e7605de1e39331347c0607024767
me3351888-lang <me3351888@gmail.com>
2026-01-22 22:34:34 +0200

    Change project base name to '/My_Blog/'
    Updated project base name to match GitHub repository.

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
 import { fileURLToPath, URL } from 'node:url'
 import tailwindcss from '@tailwindcss/vite'
 
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/my_blog_project/',
+  // التعديل هنا: غيرنا اسم المشروع ليكون مطابق لـ GitHub
+  base: '/My_Blog/', 
   plugins: [
-
     vue(),
     vueDevTools(),
     tailwindcss(),
